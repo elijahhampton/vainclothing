51c7ecee69fb14302ac70215ef1321c4d7d5cefe
Elijah Hampton <elijahhampton@Elijahs-MBP.attlocal.net>
2021-03-16 15:23:45 -0400

    Creates header component, adds logo, and styles links.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/HomePage';
 import ShopPage from './pages/shop/ShopPage';
+import Header from './components/header/Header';
 
 import { Route, Switch } from 'react-router-dom';
 
@@ -9,6 +10,7 @@ function App() {
   return (
 
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path='/shop' component={ShopPage} />
