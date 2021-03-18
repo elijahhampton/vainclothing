45ae23b7c14d025aacd60625dbfdbfa01dd13d07
Elijah Hampton <elijahhampton@Elijahs-MBP.attlocal.net>
2021-03-18 14:53:39 -0400

    Adds reselect package and adds selectors to CartIcon and CartDropdownCOmponent.

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import CartItem from '../cart-item/CartItem';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 
+import CartItem from '../cart-item/CartItem';
 import ContainedButton from '../contained-button/ContainedButton';
 
 import './styles.scss';
@@ -19,8 +20,8 @@ const CartDropdown = ({ cartItems }) => (
     </div>
 )
 
-const mapStateToProps = ({ cart: { cartItems }}) => ({
-    cartItems
+const mapStateToProps = state => ({
+    cartItems: selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropdown);
